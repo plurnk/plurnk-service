5aa772a6c9b47fc0c6b8b351b478d55783e911f2
wikitopian <wikitopian@pm.me>
2026-05-18 01:07:03 -0400

    chore: #123 schema_alignment converted (21 tests)

---
+++ test/intg/schema_alignment.sql
@@ -0,0 +1,40 @@
+-- One PREP per table for PRAGMA table_info-style introspection. PRAGMA
+-- doesn't accept bound params, but pragma_table_info() is a table-valued
+-- function so we use a per-table SELECT. We only need column name + type;
+-- the original notnull check was advisory and dropped in the SqlRite port.
+
+-- PREP: test_align_table_exists
+SELECT name FROM sqlite_master WHERE type = 'table' AND name = $name;
+
+-- PREP: test_align_cols_sessions
+SELECT name, type FROM pragma_table_info('sessions');
+
+-- PREP: test_align_cols_runs
+SELECT name, type FROM pragma_table_info('runs');
+
+-- PREP: test_align_cols_loops
+SELECT name, type FROM pragma_table_info('loops');
+
+-- PREP: test_align_cols_turns
+SELECT name, type FROM pragma_table_info('turns');
+
+-- PREP: test_align_cols_entries
+SELECT name, type FROM pragma_table_info('entries');
+
+-- PREP: test_align_cols_entry_channels
+SELECT name, type FROM pragma_table_info('entry_channels');
+
+-- PREP: test_align_cols_entry_tags
+SELECT name, type FROM pragma_table_info('entry_tags');
+
+-- PREP: test_align_cols_log_entries
+SELECT name, type FROM pragma_table_info('log_entries');
+
+-- PREP: test_align_cols_visibility
+SELECT name, type FROM pragma_table_info('visibility');
+
+-- PREP: test_align_cols_schemes
+SELECT name, type FROM pragma_table_info('schemes');
+
+-- PREP: test_align_cols_providers
+SELECT name, type FROM pragma_table_info('providers');
